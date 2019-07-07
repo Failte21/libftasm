648df90187476dfc78f42b56fefc15a00d662819
Leo SIMON <lsimon@e1r11p18.42.fr>
2019-07-07 12:32:04 +0200

    cat: working unsafe and unoptimized

--- ft_cat.s
+++ ft_cat.s
@@ -19,15 +19,20 @@ ft_cat:
 	push rdx
 	mov rax, MACH_SYSCALL(READ)
 	syscall
+	sub rsp, 8
+	push rax
 	mov rdi, 1						; STDOUT
 	mov rdx, rax
 	mov rax, MACH_SYSCALL(WRITE)
 	syscall
+	pop rax
+	add rsp, 8
 	pop rdx
 	add rsp, 8 
 
 lp:
-	jmp ft_cat
+	cmp rax, 0
+	jg ft_cat
 
 end:
 	leave
