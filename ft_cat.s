4cfac886b26e759f3fd296b3233faa84d7bb2665
Leo SIMON <lsimon@e1r11p18.42.fr>
2019-07-07 14:45:08 +0200

    cat: simpler

--- ft_cat.s
+++ ft_cat.s
@@ -1,7 +1,7 @@
 %define MACH_SYSCALL(nb)	0x2000000 | nb
 %define READ 3
 %define WRITE 4
-%define BUF_SIZE 1024
+%define BUF_SIZE 1
 %define STDOUT 1
 
 extern _ft_puts
@@ -22,31 +22,25 @@ init:
 ft_cat:
 
 read:
-	sub rsp, 8						; save buf_size
-	push rdx
+	mov rdx, BUF_SIZE				; init buf_size
 	lea rsi, [rel buffer]			; init buffer
 	mov rax, MACH_SYSCALL(READ)
 	syscall
-	pop rdx
-	add rsp, 8
+	cmp rax, 0
+	jle end
 
 write:
-	sub rsp, 32
 	push rdi						; save fd
-	push rax						; save read output
-	push rdx						; save buf_size
+	push rax
 	mov rdi, STDOUT
 	mov rdx, rax					; write as many bytes as we were able to read
 	mov rax, MACH_SYSCALL(WRITE)
 	syscall
-	pop rdi
 	pop rax
-	pop rdx
-	add rsp, 32
+	pop rdi
 
 lp:
-	; cmp rax, 0
-	; jg ft_cat
+	jg ft_cat
 
 end:
 	leave
