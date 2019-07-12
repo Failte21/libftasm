7a37631461c4b6917f9ff7f2868de4e3f40414eb
Leo SIMON <lsimon@e1r12p19.42.fr>
2019-07-12 14:40:10 +0200

    ft_strlen: More tests

--- main.c
+++ main.c
@@ -54,16 +54,40 @@ int test_strcat()
 	return (0);
 }
 
-int test_strlen()
+int _strlen(char *s)
 {
 	int	a;
 	int b;
 
-	printf("-----------STRLEN----------\n");
-	a = strlen("Hello world");
-	b = ft_strlen("Hello world");
+	a = strlen(s);
+	b = strlen(s);
 	printf("strlen: %d, ft_strlen: %d\n", a, b);
 	return a - b;
+
+}
+int _test_strlen(char *s[7], int i)
+{
+	if (i < 6)
+		return _test_strlen(s, i + 1) + _strlen(s[i]);
+	return _strlen(s[i]);
+}
+
+int test_strlen()
+{
+	int err = 0;
+	char	*strs[7] = {
+		"Hello world!\n",
+		"Open the gates !!!\n",
+		"Hat er sich von 0 auf 100 in den Raum gesetzt\n",
+		"Nunc egestas faucibus sapien ut ultrices. Curabitur convallis tellus a dignissim varius. Sed ac condimentum nulla. Proin vehicula, quam ac dictum accumsan, velit elit condimentum ipsum, ut sodales nibh urna nec ex. Nam vehicula felis nulla. Praesent scelerisque ex ipsum, sit amet facilisis lorem luctus in. Duis lacus urna, ultrices vel nunc id, consectetur sodales ante.\n",
+		"Ut scelerisque, enim nec eleifend rutrum, diam augue condimentum velit, vitae consectetur risus lacus vel ex. Fusce quis felis ut dui malesuada accumsan vehicula et erat. Suspendisse feugiat id justo nec sollicitudin. Curabitur sodales nibh neque, id fringilla nulla tincidunt nec. Suspendisse vel felis eget enim bibendum ornare. Sed pulvinar est quis eros fringilla, vitae egestas leo hendrerit. Donec rutrum consequat nunc, mattis vehicula arcu molestie id. Donec rutrum velit ut rhoncus pulvinar.\n",
+		"Nam sollicitudin faucibus nulla. In hac habitasse platea dictumst. Donec at blandit tortor, vitae molestie libero. Mauris porttitor nec est nec venenatis. Nunc tortor quam, vehicula ut malesuada in, sodales vitae enim. Donec ac placerat nunc, auctor facilisis urna. Donec blandit lectus ac nunc luctus, et bibendum est aliquam. Curabitur suscipit, est euismod tristique porta, felis mauris dignissim enim, vel mattis metus lacus et erat. Curabitur laoreet dui at lorem sollicitudin, et auctor ligula dapibus. Fusce ut dui sem. Etiam vel varius arcu. Phasellus eget tincidunt quam.",
+		"Nullam fringilla, ipsum eget venenatis vulputate, quam sem venenatis urna, a vehicula turpis est non ipsum. Integer posuere elit id nibh gravida fringilla sed non urna. Vestibulum euismod dignissim purus sit amet sodales. Vivamus scelerisque sed sapien ac euismod. Quisque volutpat leo vel consectetur ornare. Donec pulvinar, elit eu pellentesque tristique, risus nisl aliquet urna, ut ultrices ex turpis et orci. Pellentesque mattis congue nulla et maximus. Nullam accumsan auctor consectetur. Nullam cursus nisi elit, consectetur convallis diam facilisis nec. Donec a massa sit amet ipsum feugiat efficitur. Sed dapibus ullamcorper dictum. Vivamus mollis convallis purus et efficitur. Nam commodo, sapien sed finibus interdum, lorem quam vulputate leo, eget sagittis leo libero eu massa. Integer volutpat, nunc sit amet condimentum pulvinar, nibh nulla luctus diam, non consectetur justo arcu id tortor. Aliquam ut tellus a neque lobortis lobortis vel et metus."
+	};
+	printf("-----------STRLEN----------\n");
+	printf(">>>> Basic tests <<<<\n");
+	err += _test_strlen(strs, 0);
+	return err;
 }
 
 int _test_puts(char *s)
