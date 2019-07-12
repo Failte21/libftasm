f82381e9b84b1c030c60a8387735e2ea970c9b90
Leo SIMON <lsimon@e1r9p16.42.fr>
2019-07-12 14:25:34 +0200

    ft_strlen: understandable version

--- ft_strlen.s
+++ ft_strlen.s
@@ -9,11 +9,10 @@ init:
 
 lp:
 	mov al, 0
-	mov rcx, 0
-	not rcx
+	mov rcx, 0xFFFFFFFF				; rcx will count down
+	inc rcx							; don't count terminal char
 	repnz scasb
-	not rcx,
-	dec rcx
+	not rcx,						
 	mov rax, rcx
 end:
 	leave
