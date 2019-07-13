ba2cef4a9aff69e62e466c36f0e862314c8faa8d
Leo SIMON <lsimon@e1r9p6.42.fr>
2019-07-13 15:46:18 +0200

    test_cat: accept directory argument

--- test_cat.sh
+++ test_cat.sh
@@ -5,18 +5,25 @@ WHITE='\033[1;37m'
 
 gcc ft_cat.c -g libfts.a -o ft_cat
 
-for d in * ; do
-	echo "$WHITE"
-	echo "$d"
-    cat "$d" > cat.txt
-    ./ft_cat "$d" > ft_cat.txt
-	DIFF=$(diff cat.txt ft_cat.txt) 
-	if [ "$DIFF" != "" ] 
-	then
-		echo "$RED"
-		echo "KO"
-	else
-		echo "$GREEN"
-		echo "OK"
-	fi
-done
+if [ -z "$1" ]
+then
+	echo "Usage: sh test_cat.sh [dirname]"
+else
+	FILES=$1/*
+	for f in $FILES ; do
+		echo "$WHITE"
+		echo "$f"
+		cat "$f" > cat.txt
+		./ft_cat "$f" > ft_cat.txt
+		DIFF=$(diff cat.txt ft_cat.txt) 
+		if [ "$DIFF" != "" ] 
+		then
+			echo "$RED"
+			echo $DIFF
+			echo "KO"
+		else
+			echo "$GREEN"
+			echo "OK"
+		fi
+	done
+fi
